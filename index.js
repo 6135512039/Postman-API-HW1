226ec65fcb92c545e6e6c3038c94d5eb91ba7e89
6135512039 <Kongkom25@gmail.com>
2022-01-17 17:18:40 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const app = require('express')();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended:false
+}))
+app.use(cors());
+
+app.get('/get',(req,res)=>{
+    res.status(200).json({
+        name:'Kongkom Sumlee',
+        age:22,
+        faculty:'CoE'
+    });
+});
+
+app.listen(3000, (err)=> err? console.log('Error Connect Port 3000') : console.log('Connected Port 3000'));
